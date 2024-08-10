0ce90a8777c24a3a4b13d0d6d8587629b9049939
Evgeniy_Malikov <malikov.evgeniy@gmail.com>
2024-08-10 22:59:01 +0300

    final

--- config/settings.py
+++ config/settings.py
@@ -12,19 +12,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = os.getenv("SECRET_KEY")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# if os.environ.get("DEBUG") == "False":
-#     DEBUG = False
-# else:
-#     DEBUG = True
+if os.environ.get("DEBUG") == "False":
+    DEBUG = False
+else:
+    DEBUG = True
 
 # DEBUG = os.getenv("DEBUG")
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-# ALLOWED_HOSTS = ["127.0.0.1", "EvgeniyMalikov.pythonanywhere.com"]
-DEBUG = True
+ALLOWED_HOSTS = ["127.0.0.1", "EvgeniyMalikov.pythonanywhere.com"]
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "mysite.apps.MysiteConfig",
@@ -67,25 +67,25 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": os.getenv("MYSQL_DBNAME"),
-#         "USER": os.getenv("MYSQL_USER"),
-#         "PASSWORD": os.getenv("MYSQL_PASSWORD"),
-#         "HOST": os.getenv("MYSQL_HOST"),
-#         "OPTIONS": {
-#             "init_command": "SET NAMES 'utf8mb4';SET sql_mode = 'STRICT_TRANS_TABLES'",
-#             "charset": "utf8mb4",
-#         },
-#     }
-# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("MYSQL_DBNAME"),
+        "USER": os.getenv("MYSQL_USER"),
+        "PASSWORD": os.getenv("MYSQL_PASSWORD"),
+        "HOST": os.getenv("MYSQL_HOST"),
+        "OPTIONS": {
+            "init_command": "SET NAMES 'utf8mb4';SET sql_mode = 'STRICT_TRANS_TABLES'",
+            "charset": "utf8mb4",
+        },
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
